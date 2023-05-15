3d01c3b85d34f07e1a642f05288fbf0e23b305e4
yueunkim <yueunkim0605@gmail.com>
2023-05-15 21:29:14 +0900

    use react router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={App} />
+);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
